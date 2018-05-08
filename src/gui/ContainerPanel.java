add8fb3a228e043defee312b2d2729cee5b67640
killboxsarah <killbox.sarah@gmail.com>
2018-05-07 20:53:30 -0400

    model and view in presenter

--- src/gui/ContainerPanel.java
+++ src/gui/ContainerPanel.java
@@ -1,4 +1,16 @@
 package gui;
 
-public class ContainerPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ContainerPanel extends JPanel {
+
+    private MenuItemPanel menuItemPanel;
+
+    public ContainerPanel () {
+        this.setName("Container Panel");
+        this.menuItemPanel = new MenuItemPanel();
+        this.add(menuItemPanel);
+    }
+
 }
--- test/gui/ContainerPanelTest.java
+++ test/gui/ContainerPanelTest.java
@@ -1,7 +1,31 @@
 package gui;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
 import static org.junit.Assert.*;
 
 public class ContainerPanelTest {
 
+    private ContainerPanel containerPanel;
+
+    @Before
+    public void setUpForContainerPanel(){
+        containerPanel = new ContainerPanel();
+    }
+
+    @Test
+    public void nameOfContainerPanel() {
+        assertEquals("Container Panel",containerPanel.getName());
+
+    }
+
+    @Test
+    public void canHoldAPanel () {
+        Component[] items = containerPanel.getComponents();
+        assertEquals("Menu Item Panel",items[0].getName());
+
+    }
 }
