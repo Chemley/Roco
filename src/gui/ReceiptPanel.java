3bc0a1c74e31408cca0f8753d271a39d13414bcc
killboxsarah <killbox.sarah@gmail.com>
2018-05-09 18:51:01 -0400

    created jar file

--- src/gui/ReceiptPanel.java
+++ src/gui/ReceiptPanel.java
@@ -10,14 +10,13 @@ public class ReceiptPanel extends JPanel {
 
     public ReceiptPanel() {
         this.setName("Receipt Panel");
-        this.tacoButton.setName("Taco Button");
+  /*      this.tacoButton.setName("Taco Button");
         this.tacoButton.setText("Taco Button");
-        this.add(tacoButton);
+        this.add(tacoButton);*/
         this.setBackground(Color.CYAN);
     }
-
+/*
     public JButton getTacoButton() {
-        return this.tacoButton;
+        return this.tacoButton;*/
 
     }
-}
